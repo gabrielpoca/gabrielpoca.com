26bb4367b581c29f6ce37d62f150cbc4cbb25f7e
Gabriel Poca <mail@gabrielpoca.com>
2021-11-21 00:01:14 +0000

    Remove wavesurfer to allow streaming of audio files

--- priv/site/music/index.js
+++ priv/site/music/index.js
@@ -1,62 +1,21 @@
-const playerEl = document.querySelector("#Player-wave");
-
-let wavesurfer;
-
 document.addEventListener("alpine:init", () => {
   Alpine.store("player", {
     src: null,
     title: null,
-    isLoading: false,
     isPlaying: true,
-    set(src, title) {
+    async set(el, src, title) {
       if (src === this.src) {
-        wavesurfer.playPause();
+        if (el.paused) await el.play();
+        else el.pause();
       } else {
-        onPlay(src);
-        this.isLoading = true;
         this.src = src;
         this.title = title;
+
+        el.currentTime = 0;
+        await el.play();
       }
-    },
-    toggle() {
-      wavesurfer.playPause();
+
+      this.isPlaying = !el.paused;
     },
   });
 });
-
-function onPlay(src) {
-  setupWavesurfer();
-
-  wavesurfer.load(src);
-}
-
-function setupWavesurfer() {
-  if (wavesurfer) return;
-
-  wavesurfer = WaveSurfer.create({
-    container: playerEl,
-    waveColor: "#fca5a5",
-    progressColor: "#cb90f9",
-    cursorColor: "#cb90f9",
-    mediaControls: true,
-  });
-
-  wavesurfer.on("play", () => {
-    Alpine.store("player").isPlaying = true;
-  });
-
-  wavesurfer.on("pause", () => {
-    Alpine.store("player").isPlaying = false;
-  });
-
-  wavesurfer.on("finish", () => {});
-
-  wavesurfer.on("ready", () => {
-    Alpine.store("player").isLoading = false;
-    wavesurfer.play();
-  });
-}
-
-//if (import.meta.hot) {
-//import.meta.hot.accept(() => window.location.reload());
-//}
